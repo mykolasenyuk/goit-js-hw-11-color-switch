956bf2c61d399c8c7c48381c3edfce3e6db7ff56
mykolasenyuk <73036879+mykolasenyuk@users.noreply.github.com>
2021-05-03 21:09:00 +0100

    created

---
+++ index.js
@@ -0,0 +1,42 @@
+const colors = [
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+];
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const refs = {
+    buttonStart: document.querySelector('[data-action="start"]'),
+    buttonStop: document.querySelector('[data-action="stop"]')
+}
+// console.log(refs.buttonStart)
+const DELAY = 1000;
+let interval = undefined
+refs.buttonStart.addEventListener('click', onStartButtonClick);
+refs.buttonStop.addEventListener('click',onStopButtonClick)
+
+function onStartButtonClick() {
+
+ changeColors()
+}
+
+function onStopButtonClick() {
+    clearInterval(interval)
+    refs.buttonStart.removeAttribute('disabled', '');
+}
+
+function changeColors() {
+    refs.buttonStart.setAttribute('disabled', '')
+    interval = setInterval(() => { 
+    document.body.style.backgroundColor=colors[randomIntegerFromInterval(0, colors.length - 1)]
+},DELAY)
+}
+
+
+
