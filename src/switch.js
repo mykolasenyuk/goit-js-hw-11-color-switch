0da80b327143c00594fe6498db9a03fefc460515
mykolasenyuk <73036879+mykolasenyuk@users.noreply.github.com>
2021-05-08 15:46:43 +0100

    Update switch.js

--- src/switch.js
+++ src/switch.js
@@ -1,16 +1,24 @@
 const colors = [
-  '#FFFFFF',
-  '#2196F3',
-  '#4CAF50',
-  '#FF9800',
-  '#009688',
-  '#795548',
-  '#ADFF2F',
+ 
   '#FF0000',
   '#FF4500',
   '#0000CD',
   '#FFFF00',
   '#DA70D6',
+  '#4158D0',
+  '#FFCC70',
+  'linear-gradient(43deg, #4158D0 0%, #C850C0 46%, #FFCC70 100%)',
+  'linear-gradient(90deg, #00DBDE 0%, #FC00FF 100%)',
+  'linear-gradient(45deg, #85FFBD 0%, #FFFB7D 100%)',
+  'linear-gradient(62deg, #FBAB7E 0%, #F7CE68 100%)',
+  'linear-gradient(45deg, #FA8BFF 0%, #2BD2FF 52%, #2BFF88 90%)',
+  'linear-gradient(45deg, #FBDA61 0%, #FF5ACD 100%)',
+  'linear-gradient(45deg, #FFFFFF 0%, #4CAF50 100%)',
+  'linear-gradient(45deg, #FA8BFF 0%, #DA70D6 52%, #4CAF50 90%)',
+  'linear-gradient(225deg, #FF3CAC 0%, #784BA0 50%, #2B86C5 100%)',
+
+
+
   
 ];
 
@@ -47,7 +55,7 @@ function changeColors() {
   refs.buttonStart.classList.toggle('deactive');
   
     interval = setInterval(() => { 
-      document.body.style.backgroundColor = colors[randomIntegerFromInterval(0, colors.length - 1)]
+      document.body.style.background = colors[randomIntegerFromInterval(0, colors.length - 1)]
 },DELAY)
 }
 
